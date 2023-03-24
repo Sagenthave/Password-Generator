79b1e990cf1fee1532d671d06ca5bcbc467df94d
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-03-23 22:32:44 -0400

    reorganized the functions and the vairables to make sense. Added a readme section.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Password-Generator
+
+## Project Objective 
+This site is designed to generate a random password with the critera requested by the user. 
+
+![screenshot](/Screenshot%202023-03-23%20at%202.25.05%20PM.png)
+
+## Changes Created 
+- Added function generatePassword
+- Added acceptance criteria for selecting options for the user password
+
+## Credits 
+Starter Code obtained from: https://github.com/coding-boot-camp/friendly-parakeet 
+
+Assistance: Tutor Faran Navazi 
+
+## Deployment 
+- Deployed URl: 
+- gitHub URL: https://github.com/Sagenthave/Password-Generator
--- script.js
+++ script.js
@@ -1,90 +1,134 @@
 // Assignment code here
 
-var length;
+//Kept these options Global so all function can access them
 var confirmNumber;
-var confirmCharacter;
+var confirmSymbol;
 var confirmUpperLetter;
 var confirmlowerLetter;
-var password = []
-
+var resultPassword = [];
 symbols = ["@", "!", "#", "$", "%", "^", "&", "*", " ?"];
 lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 numbers = ["0","1","2","3","4","5","6","7","8","9",];
 
-function passwordOptions() { //originally has it has generate password
-  length = parseInt(prompt("How long would you like your password?"));
-  if (!lenght|| characterLength < 8 || characterLength > 128) {
-    alert("Enter valid number!")
-  } else {
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length)
+  var randomElement = arr[randomIndex];
+  return randomElement
+}
+
+function getPasswordOptions() { 
+  var length = parseInt(prompt("How long would you like your password? 8-128"));
+  if (!length|| length < 8 || length > 128) {
+    alert("Enter valid number between 8-128!")
+    return null
+  } 
   // add confirm here 4 (Tutor notes)
     confirmNumber = confirm("Would you like numbers in your password?");
-    confirmCharacter = confirm("Would you like symbols in your password?");
+    confirmSymbol = confirm("Would you like symbols in your password?");
     confirmUpperLetter = confirm("Would you like uppercase letters in your password?");
     confirmlowerLetter = confirm("Would you like lowercase letters in your password?");
-  };
-  if (!confirmCharacter && !confirmNumber && !confirmUpperLetter && !confirmlowerLetter &&) {
-    option = alert ("Please try again")
-  }
+    if (!confirmSymbol && !confirmNumber && !confirmUpperLetter && !confirmlowerLetter) {
+      alert ("Please try again")
+      return null;
+    }
+    var passwordOptions = {
+      length: length,
+      confirmNumber: confirmNumber,
+      confirmSymbol: confirmSymbol,
+      confirmUpperLetter: confirmUpperLetter,
+      confirmlowerLetter: confirmlowerLetter
+    };
+  return passwordOptions;
+}
 // All 4 selected
-  else if (confirmCharacter && confirmNumber && confirmUpperLetter && confirmlowerLetter) {
-    option = concat(symbols, lowerLetters, upperLetters, numbers);
-  }
-// 3/4 options selected
-  else if (confirmCharacter && confirmNumber && confirmUpperLetter) {
-    option = concat(symbols, numbers, upperLetters)
-  }
-  else if (confirmCharacter && confirmNumber && confirmlowerLetter) {
-    option = concat(symbols, numbers, lowerLetters)
-  }
-  else if (confirmCharacter && confirmUpperLetter && confirmlowerLetter) {
-    option = concat(symbols, upperLetters, lowerLetters)
-  }
-  else if (confirmNumber && confirmUpperLetter && confirmlowerLetter) {
-    option = concat(numbers, upperLetters, lowerLetters)
-  }
-// 2/4 options selected
-  else if (confirmCharacter && confirmNumber) {
-    option = concat(symbols, numbers)
-  }
-  else if (confirmCharacter && confirmUpperLetter) {
-    option = concat(symbols, upperLetters)
-  }
-  else if (confirmCharacter && confirmlowerLetter) {
-    option = concat(symbols && lowerLetters)
-  }
-  else if (confirmNumber && confirmUpperLetter) {
-    option = concat(numbers, upperLetters)
-  }
-  else if (confirmNumber && confirmlowerLetter) {
-    option = concat(numbers, lowerLetters)
-  }
-  else if (confirmUpperLetter && confirmlowerLetter) {
-    option = concat(upperLetters && lowerLetters)
-  }
-// 1/4 options selected
-  else if (confirmCharacter) {
-    option = symbols
-  }
-  else if (confirmNumber) {
-    option = numbers
-  }
-  else if (confirmUpperLetter) {
-    option = upperLetters
-  }
-  else if (confirmlowerLetter) {
-    option = lowerLetters
-  }
-};
 
 function generatePassword () {
-
-}
-//Tutor taught me this but I don't know what to do here
-var option = password.join("") 
+  var options = getPasswordOptions();
+  var resultPassword = [];
+  var guranteedChar = [];
+  var passwordCritera = [];
+  if(!options) return null;
+  if(options.confirmNumber){
+    passwordCritera = passwordCritera.concat(numbers);
+    guranteedChar.push(getRandom(numbers))
+  }
+  if (options.confirmSymbol){
+    passwordCritera= passwordCritera.concat(symbols);
+    guranteedChar.push(getRandom(symbols))
+  }
+  if (options.confirmUpperLetter){
+    passwordCritera= passwordCritera.concat(upperLetters);
+    guranteedChar.push(getRandom(upperLetters))
+  }
+  if (option.confirmlowerLetter){
+    passwordCritera=passwordCritera.concat(lowerLetters);
+    guranteedChar.push(getRandom(lowerLetters))
+  }
+  if (confirmSymbol && confirmNumber && confirmUpperLetter && confirmlowerLetter) {
+    option = guranteedChar.push(symbols, lowerLetters, upperLetters, numbers);
+  }
+// 3/4 options selected
+  if (confirmSymbol && confirmNumber && confirmUpperLetter) {
+    option = guranteedChar.push(symbols, numbers, upperLetters)
+  }
+  if (confirmSymbol && confirmNumber && confirmlowerLetter) {
+    option = guranteedChar.push(symbols, numbers, lowerLetters)
+  }
+  if (confirmSymbol && confirmUpperLetter && confirmlowerLetter) {
+    option = guranteedChar.push(symbols, upperLetters, lowerLetters)
+  }
+  if (confirmNumber && confirmUpperLetter && confirmlowerLetter) {
+    option = guranteedChar.push(numbers, upperLetters, lowerLetters)
+  }
+// 2/4 options selected
+  if (confirmSymbol && confirmNumber) {
+    option = guranteedChar.push(symbols, numbers)
+  }
+  if (confirmSymbol && confirmUpperLetter) {
+    option = guranteedChar.push(symbols, upperLetters)
+  }
+  if (confirmSymbol && confirmlowerLetter) {
+    option = guranteedChar.push(symbols && lowerLetters)
+  }
+  if (confirmNumber && confirmUpperLetter) {
+    option = guranteedChar.push(numbers, upperLetters)
+  }
+  if (confirmNumber && confirmlowerLetter) {
+    option = guranteedChar.push(numbers, lowerLetters)
+  }
+  if (confirmUpperLetter && confirmlowerLetter) {
+    option = guranteedChar.push(upperLetters && lowerLetters)
+  }
+// 1/4 options selected
+  if (confirmSymbol) {
+    option = guranteedChar.push(symbols)
+  }
+  if (confirmNumber) {
+    option = guranteedChar.push(numbers)
+  }
+  if (confirmUpperLetter) {
+    option = guranteedChar.push(upperLetters)
+  }
+  if (confirmlowerLetter) {
+    option = guranteedChar.push(lowerLetters)
+  }
+};
+  var password = resultPassword + passwordOptions;
+  for (var i = 0; i < length; i++) {
+    console.log("Your random password is: " + resultPassword)
+  }
+  var possibleChar = ""
+  for (var i =0; i < length; i++) {
+    var pickvariables = option[Math.floor(Math.random() * option.length)];
+    password.push(pickvariables);
+    var option = password.join("") 
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
