b256350f82d4dab336e25fc7c9b225724169271e
Sagenthave <125151569+Sagenthave@users.noreply.github.com>
2023-03-24 16:32:50 -0400

    spelling errors

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Project Objective 
 This site is designed to generate a random password with the critera requested by the user. Options for the password include length (8-128 characters), symbols, numbers and letters. A new password will be generated at random with no noticable repeats. 
 
-![screenshot](/Screenshot%202023-03-23%20at%202.25.05%20PM.png)
+![screenshot](/Screenshot%202023-03-24%20at%204.28.54%20PM.png)
 
 ## Changes Created 
 - Added function generatePassword
@@ -19,5 +19,5 @@ Starter Code obtained from: https://github.com/coding-boot-camp/friendly-parakee
 
 
 ## Deployment 
-- Deployed URl: https://sagenthave.github.io/Password-Generator/
+- Deployed URL: https://sagenthave.github.io/Password-Generator/
 - gitHub URL: https://github.com/Sagenthave/Password-Generator
--- script.js
+++ script.js
@@ -8,24 +8,27 @@ var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
 var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
+//Created generate password function that was missing in the starter code
 function generatePassword() {
   var userLength = ""; 
   var inputLength = window.prompt("What length would you like for your password? 8-128 characters.");
-  Number(inputLength);
+  Number(inputLength); //Insert a value to generate a password length
   if (inputLength < 8 || inputLength > 128) {
     alert ("Please enter a valid number from 8-128"); 
-    return;
+    return; // if invalid number was entered the code will not continue
   } else if (inputLength <= 128 && inputLength >= 8 ) {
     userLength=inputLength;
   } else {
     return;
   }
-  var charArr = [];
-  var newPassword = "";
+  var charArr = []; //array for randomized characters
+  var newPassword = ""; //for the new generated password
+  //four confirms 
   var confirmSymbols = window.confirm("Would you like symbols in your password?");
   var confirmNumbers = window.confirm("Would you like numbers in your password?");
   var confirmLowerLetters = window.confirm("Would you like lowercase letters in your password?");
   var confirmUpperLetters = window.confirm("Would you like uppercase letters in your password?");
+  //pushing 
   if (confirmSymbols) {
     charArr.push("@", "!", "#", "$", "%", "^", "&", "*", "?"); 
   }
@@ -40,13 +43,12 @@ function generatePassword() {
   } else if (!confirmSymbols && !confirmNumbers && !confirmLowerLetters && !confirmUpperLetters) {
     window.alert("Invalid Response: Please select a valid input");
   } 
-
+//For loop will generate a random character and add it to the empty newPassword string
   for (i=0; i < userLength; i++) {
     var randomChar = charArr[Math.floor(Math.random() * charArr.length)];
     newPassword = newPassword + randomChar;
   }
   return newPassword;
-  console.log(newPassword);
 } 
 
 
